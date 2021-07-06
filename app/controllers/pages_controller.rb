494a1a8baca8c36316f38cee2d8549f12546cfac
Austin Nash <austinnash@Austins-MacBook-Pro.local>
2021-07-06 11:00:28 -0700

    marketing

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,12 @@
 class PagesController < ApplicationController
   def home
-    if current_user.present? && current_user.admin? == false
+    if current_user.present? && current_user.mktg? && current_user.admin? == false
+      redirect_to pages_marketing_path()
+    elsif current_user.present? && current_user.admin? == false
       redirect_to pages_customer_service_path()
     end
   end
+  
   def customer_matching_cpa
     @master_cpa_total = MasterCpa.count
     @master_cpa_match_total = MasterCpaMatch.count
